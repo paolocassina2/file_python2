b2690f207164b6aed8c493799c6e3ea2092764a9
unknown <paolo.cassina91@gmail.com>
2022-04-07 13:52:41 +0200

    sortdirectory19.py sposta file nell'ultima cartella che trova

---
+++ file_python/prova/chr.py
@@ -0,0 +1,14 @@
+###CARTELLA==prova
+# ~ for x in range(1,1001):
+c=0
+ciao=""
+while True:
+	c=c+1
+	print(chr(c))
+	print(c)
+
+
+	ciao=ciao+chr(c)
+	if c==50:
+		break
+print("ciao",ciao)
---
+++ file_python/prova/speedtyping.py
@@ -0,0 +1,39 @@
+###CARTELLA==prova
+import time
+# ~ while True:
+	# ~ input()
+#inizia a calcolare il tempo dall'inizio del programma
+# ~ for x in a:
+	# ~ x=input("write text")
+	# ~ print(x)
+	
+# ~ for x in a: 
+	# ~ print(a)
+# ~ if a=="":
+	# ~ print("you pressed enter")
+# ~ for x in a:
+import time
+# ~ import time
+	# ~ if x!=""0
+start = time.time()
+print(start)
+a=input()
+# ~ if len(a)>0:
+
+	# ~ print(start)
+
+
+	# ~ print("hello")
+# ~ if x==""
+		
+	# ~ time.sleep(4)
+	# ~ if len(a)>=10:
+end = time.time()
+# ~ elapsed=end - start
+# ~ from datetime import timedelta
+# ~ tempo=str(timedelta(seconds=elapsed))
+# ~ print(end-start,"tempo trascorso")
+print("caratteri_al_secondo ", len(a)/(end-start))
+	# ~ print(start)
+# ~ print(end - start)
+# ~ print("velocità di scrittura (caratteri al secondo) ",len(a)/(end-start))
